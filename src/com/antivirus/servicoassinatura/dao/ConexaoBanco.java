4980599139246d4ed2184b57f909547836feade1
Bruno <brunopereira900@outlook.com>
2025-10-11 19:51:38 -0300

    Removendo arquivos de configuração da IDE do versionamento

--- src/com/antivirus/servicoassinatura/dao/ConexaoBanco.java
+++ src/com/antivirus/servicoassinatura/dao/ConexaoBanco.java
@@ -1,4 +1,9 @@
 package com.antivirus.servicoassinatura.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoBanco {
+    private
 }
